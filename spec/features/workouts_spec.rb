6ce043edbb0837e538be32c5a94a2d3ecc68ad26
Andrew Tomaka <atomaka@gmail.com>
2013-08-10 02:11:11 -0400

    Don't need to check status code

--- spec/features/workouts_spec.rb
+++ spec/features/workouts_spec.rb
@@ -6,11 +6,6 @@ describe "Workouts" do
   end
 
   describe "GET /workouts" do
-    it "should return 200" do 
-      visit workouts_path
-      page.status_code.should be 200
-    end
-
     it "should return workouts" do
       visit workouts_path
       page.should have_content @workout.date
@@ -26,11 +21,6 @@ describe "Workouts" do
       page.should have_content @workout.date
     end
 
-    it "should return 200" do
-      visit "#{workouts_path}/#{@workout.id}"
-      page.status_code.should be 200
-    end
-
     it "should return the correct workout" do
       visit "#{workouts_path}/#{@workout.id}"
       page.should have_content @workout.date
@@ -41,7 +31,7 @@ describe "Workouts" do
     it "should edit the workout" do
       visit workouts_path
       find("#workout_#{@workout.id}").click_link 'Edit'
-      
+
       current_path.should == edit_workout_path(@workout)
     end
   end
