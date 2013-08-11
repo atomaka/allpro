966f734aa03b24ceb4af651d08087df57cdfd667
Andrew Tomaka <atomaka@gmail.com>
2013-08-11 13:40:44 -0400

    Use symbols for day_types

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,13 @@
 class Workout < ActiveRecord::Base
-  DAY_TYPES = %w(heavy medium light)
+  DAY_TYPES = [:heavy, :medium, :light ]
   validates_date :date
   validates_inclusion_of :day_type, :in => Workout::DAY_TYPES
+
+  def day_type
+    read_attribute(:day_type).to_sym
+  end
+
+  def day_type=(value)
+    write_attribute(:day_type, value.to_s)
+  end
 end
